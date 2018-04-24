a4088eb329a30d096e462c7226523d2e49a42b8c
André Alencar <andreluis88@gmail.com>
2018-04-24 13:34:11 -0300

    Change home for logged user
    - When user is logged, home page is his lists
    - When user is not logged, home page is the landing page

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
+require 'devise_support'
 
 RSpec.describe HomeController, type: :controller do
+  include DeviseSupport
+  describe '#index' do
+    subject { get :index }
 
+    context 'when user is not logged' do
+      it 'render home/index template' do
+        expect(subject).to render_template("home/index")
+      end
+    end
+
+    context 'when user is logged' do
+      before(:each) { login(FactoryBot.create(:user)) }
+
+      it 'redirect to lists/mine' do
+        expect(subject).to redirect_to(lists_mine_index_path)
+      end
+    end
+  end
 end
